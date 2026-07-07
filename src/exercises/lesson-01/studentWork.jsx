73049ec0a335bcf7b408ee8699a694f154eb7766
tcmorrison56 <155089594+tcmorrison56@users.noreply.github.com>
2026-07-06 21:20:36 -0700

    set up about me and hobbies section with headings

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -3,10 +3,34 @@
 
 export default function StudentWork() {
   //add variables here
+  const fullName = 'Tim Morrison';
+  const age = 32;
+  const hobbies = [
+    'fitness',
+    'kickball',
+    'softball',
+    'Minecraft',
+    'home automation',
+  ];
+
   return (
     <div>
       {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>About Me</h1>
+      <p>
+        My name is {fullName} and I am currently a coding student learning
+        React. I am {age} years old. I have a bachelors degree in Product
+        Design, and would love to incorporate my past experience into my coding.
+        I enjoy learning about new things and have spent a good amount of time
+        learning HTML, CSS, and JavaScript in the past using several Udemy
+        classes as well as the intro course from Code the Dream.
+      </p>
+      <h2>Hobbies</h2>
+      <ul>
+        {hobbies.map((hobby) => (
+          <li key={hobbies.indexOf(hobby)}>{hobby}</li>
+        ))}
+      </ul>
     </div>
   );
 }
